257f3310602f1bf77d8530e6f40c5ef204398b25
Uttam Gupta <uttamgupta0908@gmail.com>
2025-12-23 17:40:17 +0530

    feat: Introduce user profile feature along with foundational UI, layout, and data fetching utilities.

---
+++ src/features/profile/index.ts
@@ -0,0 +1,7 @@
+export { default as ProfileContainer } from './components/ProfileContainer';
+export { default as UserProfileHeader } from './components/UserProfileHeader';
+export { default as UserStats } from './components/UserStats';
+export { default as ProfileTabs } from './components/ProfileTabs';
+export { default as ProfilePostsGrid } from './components/ProfilePostsGrid';
+export { useUserProfile, useUserPosts, useFollowUser } from './hooks/useProfile';
+export * from './services';
