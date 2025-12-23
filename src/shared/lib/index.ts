6bea7a658a90e5a9591a24ceb9149743be585c2f
Uttam Gupta <uttamgupta0908@gmail.com>
2025-12-23 18:10:41 +0530

    feat: Add initial shared infrastructure including React Query setup, global styles, core types, and application constants.

---
+++ src/shared/lib/index.ts
@@ -0,0 +1,4 @@
+export { queryClient, queryKeys } from './query-client';
+export { apiClient, get, post, put, del } from './api-client';
+export { default as QueryProvider } from './QueryProvider';
+export * from './constants';
