29a2c4099d166921921410bcbc773966228997f1
Uttam Gupta <uttamgupta0908@gmail.com>
2025-12-23 17:28:12 +0530

    feat: Establish initial application structure and implement core features including feed, profile, communities, live, and messages.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Feed from "@/components/features/feed/Feed";
-
-export default function Home() {
-    return (
-        <div className="flex-1 min-h-screen bg-neutral-400 ">
-            <Feed />
-        </div>
-    );
-}
