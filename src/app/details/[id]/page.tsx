34c3cbb49f799723f0dfc95d0750ac728ef27e70
Uttam Gupta <uttamgupta0908@gmail.com>
2026-01-04 22:16:09 +0530

    feat: Implement core application features including authentication, user profiles, post feed, and foundational UI components.

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,15 +1,10 @@
-import { posts } from '@/src/shared/data/mock';
 import PostDetailView from '@/src/features/feed/components/PostDetailView';
 import { notFound } from 'next/navigation';
+import { fetchPostById } from '@/src/features/feed/services';
 
 export default async function PostDetails({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
-    const post = posts.find((p) => p.id === id);
 
-    if (!post) {
-        notFound();
-    }
-
-    return <PostDetailView post={post} />;
+    return <PostDetailView id={id} />;
 }
 
