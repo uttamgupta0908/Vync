9ad3bf06b797ebfc030072c05a50373af2f39929
Uttam Gupta <uttamgupta0908@gmail.com>
2026-01-08 23:26:52 +0530

    add appheader

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from 'react';
 import { LiveContainer } from '@/src/features/live';
+import AppHeader from '@/src/shared/layout/AppHeader';
 
 export default function LivePage() {
     return (
-        <div className="flex flex-col h-screen overflow-hidden bg-neutral-400">
+        <div className="min-h-screen bg-neutral-400 text-neutral-800 w-full">
+            <AppHeader />
             <LiveContainer />
         </div>
     );
