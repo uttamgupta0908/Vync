a57cfa8e2ce9fee07e3ae3c09ccd6c311269010c
Uttam Gupta <uttamgupta0908@gmail.com>
2026-01-08 23:27:18 +0530

    add icon in trending icon image

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from 'react';
 import { MessagesContainer } from '@/src/features/messages';
+import AppHeader from '@/src/shared/layout/AppHeader';
 
 export default function MessagesPage() {
     return (
-        <div className="flex flex-col h-screen overflow-hidden bg-neutral-400 ">
+        <div className="min-h-screen bg-neutral-400 text-neutral-800 w-full">
+            <AppHeader />
             <MessagesContainer />
         </div>
     );
