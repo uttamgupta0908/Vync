dec486e4c1c924379c49c360e62a39aec4101acf
Uttam Gupta <uttamgupta0908@gmail.com>
2026-01-09 17:57:58 +0530

    removal of unused inport

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,6 +1,4 @@
 import PostDetailView from '@/src/features/feed/components/PostDetailView';
-import { notFound } from 'next/navigation';
-import { fetchPostById } from '@/src/features/feed/services';
 
 export default async function PostDetails({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
